2108f05dbd336f079438afbbc0db970c97534aae
Nick Santos <nicholas.j.santos@gmail.com>
2019-02-05 00:00:29 -0500

    add a scroll animation and disable font ligatures

---
+++ src/assets/js/fx.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function() {
+  let lastScrollY = -1
+  let lastScrollDownTime = 0
+  let timer = 0
+
+  function $(sel) {
+    return document.querySelector(sel)
+  }
+
+  function onTimer() {
+    $('.dotSpinner').classList.remove('is-active')
+  }
+
+  window.addEventListener('scroll', () => {
+    let curScrollY = window.scrollY
+    let isDownwards = lastScrollY != -1 && curScrollY > lastScrollY
+    if (isDownwards) {
+      $('.dotSpinner').classList.add('is-active')
+      lastScrollDownTime = Date.now()
+      clearTimeout(timer)
+      timer = setTimeout(onTimer, 200)
+    }
+    lastScrollY = curScrollY
+  }, {'passive': true})
+})()
