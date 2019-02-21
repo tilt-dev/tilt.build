12e46b7b18baf85e2af544f9216865e50af34982
Matt Landis <landism@users.noreply.github.com>
2019-02-21 14:37:18 -0500

    k8s_yaml example: fix typo (#57)

--- api/api.py
+++ api/api.py
@@ -107,11 +107,11 @@ def k8s_yaml(yaml: Union[str, List[str], LocalPath, Yaml]) -> None:
 
     # LocalPath
     repo = local_git_repo('./my_proj')
-    k8s_yaml(repo.path('deploy/foo.yaml')
+    k8s_yaml(repo.path('deploy/foo.yaml'))
 
     # Yaml object (in this case, script output)
     templated_yaml = local('./template_yaml.sh')
-    k8s_yaml = (yaml(templated_yaml))
+    k8s_yaml(yaml(templated_yaml))
 
   Args:
     yaml: Path(s) to YAML, or YAML as a ``Yaml`` object.
