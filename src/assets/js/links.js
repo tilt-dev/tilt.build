011df56f94be3fe7182f1bc52236507f3b83f0f3
Nick Santos <nicholas.j.santos@gmail.com>
2019-02-20 19:02:55 -0500

    add permalink arrows to faq

---
+++ src/assets/js/links.js
@@ -0,0 +1,19 @@
+// Adapted from
+// http://blog.parkermoore.de/2014/08/01/header-anchor-links-in-vanilla-javascript-for-github-pages-and-jekyll/
+
+(function() {
+  var anchorForId = function (id) {
+    var anchor = document.createElement("a");
+    anchor.className = "permalink";
+    anchor.href      = "#" + id;
+    anchor.innerHTML = "►";
+    return anchor;
+  };
+
+  document.onreadystatechange = function () {
+    if (this.readyState === "complete") {
+      let links = Array.from(document.querySelectorAll('.docsContent h3[id]'))
+      links.forEach((el) => el.insertBefore(anchorForId(el.id), el.firstChild))
+    }
+  };
+})()
