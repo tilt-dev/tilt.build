7186e8c79f13828aff7d69724f9230e4bec0eb33
Dan Miller <dmiller@windmill.engineering>
2019-05-28 10:57:10 -0400

    api: note that fast_build is deprecated rather than linking to non-existant docs (#155)
    Fixes #153

--- api/api.py
+++ api/api.py
@@ -108,7 +108,7 @@ class FastBuild:
 def fast_build(img_name: str, dockerfile_path: str, entrypoint: str = "") -> FastBuild:
   """Initiates a docker image build that supports ``add`` s and ``run`` s, and that uses a cache for subsequent builds.
 
-    See the `fast build documentation <https://docs.tilt.dev/fast_build.html>`_.
+  **Note**: this is a deprecated feature. For the fast building of the future, check out our `LiveUpdate tutorial </live_update_tutorial.html>`_ and `reference documention </live_update_reference.html>`_.
   """
   pass
 
