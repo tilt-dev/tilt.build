4ccf5a3f861b1d100f180526a76edcb6ebc900b0
Maia McCormick <maia@windmill.engineering>
2019-09-09 11:33:58 -0400

    blog: august commit of the month (protect production cluster) (#267)
    * blog: august commit of the month (protect production cluster)
    
    * post-date blog post, fix em dashes

--- api/api.py
+++ api/api.py
@@ -551,6 +551,7 @@ def allow_k8s_contexts(contexts: Union[str, List[str]]) -> None:
   To help reduce the chances you accidentally use Tilt to deploy to your
   production cluster, Tilt will error unless at least one of these is true of
   the active K8S context (i.e., what is returned by `kubectl config current-context`)
+
   1. The K8S API URL is on localhost.
   2. The context name is one of a few known local context names (e.g,. "minikube").
   3. The context name is explicitly passed to `allow_k8s_contexts` in the Tiltfile.
