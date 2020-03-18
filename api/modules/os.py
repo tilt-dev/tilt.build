74104d3605affa3e83562d9aef05bc21136f8b0c
Dan Miller <dmiller@windmill.engineering>
2020-03-18 18:31:13 -0400

    hmm this doesnt work

--- api/modules/os.py
+++ api/modules/os.py
@@ -26,7 +26,3 @@ def getcwd() -> str:
   returns the directory of the loaded/included Tiltfile.
   """
   pass
-
-def realpath(path: str) -> str:
-  """Return the canonical path of the specified filename, eliminating any symbolic links encountered in the path (if they are supported by the operating system)."""
-  pass
---
+++ api/modules/os/path.py
@@ -0,0 +1,3 @@
+def realpath(path: str) -> str:
+  """Return the canonical path of the specified filename, eliminating any symbolic links encountered in the path (if they are supported by the operating system)."""
+  pass
