f34e2609140ac5b74d9ba628ef74e49cf71a7904
Victor Wu <victor@tilt.dev>
2020-04-30 15:51:22 -0400

    Updates

--- api/modules/os/path.py
+++ api/modules/os/path.py
@@ -1,3 +1,23 @@
+def abspath(path: str) -> str:
+  """Return a normalized absolutized version of the pathname path."""
+  pass
+
+def basename(path: str) -> str:
+  """Return the base name of pathname path."""
+  pass
+
+def dirname(path: str) -> str:
+  """Return the directory name of pathname path."""
+  pass
+
+def exists(path: str) -> str:
+  """Return True if path refers to an existing path or an open file descriptor. Returns False for broken symbolic links."""
+  pass
+
+def join(path: str) -> str:
+  """Join one path component intelligently."""
+  pass
+
 def realpath(path: str) -> str:
   """Return the canonical path of the specified filename, eliminating any symbolic links encountered in the path (if they are supported by the operating system)."""
   pass
