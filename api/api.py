e872540ae768080636c705169de42cb25b8f31b5
Maia McCormick <maia@windmill.engineering>
2020-05-14 18:00:50 -0400

    api: document secret_settings (#574)

--- api/api.py
+++ api/api.py
@@ -799,13 +799,27 @@ def struct(**kwargs) -> Any:
     print("%s %d" % (x.a, x.b)) # prints "foo 6"
   """
 
+
+def secret_settings(disable_scrub: bool = False) -> None:
+  """Configures Tilt's handling of Kubernetes Secrets. By default, Tilt scrubs
+  the text of any Secrets from the logs; e.g. if Tilt applies a Secret with contents
+  'mysecurepassword', Tilt redacts this string if ever it appears in the logs,
+  to prevent users from accidentally sharing sensitive information in snapshots etc.
+
+  If you need more other configuration options, `let us know <https://github.com/windmilleng/tilt/issues>`_.
+
+  Args:
+    disable_scrub: if True, Tilt will *not* scrub secrets from logs.
+"""
+
+
 def update_settings(max_parallel_updates: int) -> None:
   """Configures Tilt's updates to your resources. (An update is any execution of or
   change to a resource. Examples of updates include: doing a docker build + deploy to
   Kubernetes; running a live update on an existing container; and executing
   a local resource command).
 
-  Expect more settings to be configurable from this function soon.
+  If you need more other configuration options, `let us know <https://github.com/windmilleng/tilt/issues>`_.
 
   Args:
     max_parallel_updates: maximum number of updates Tilt will execute in parallel. Default is 3. Must be a positive integer.
