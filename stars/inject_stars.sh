4748994cbe2277ecc699459909becb635213a587
Nick Santos <nicholas.j.santos@gmail.com>
2020-06-03 17:02:13 -0400

    footer2: more mobile responsive

--- stars/inject_stars.sh
+++ stars/inject_stars.sh
@@ -5,5 +5,4 @@ cd $(dirname $0)
 set -ex
 yarn install
 STARS=$(node stars.js)
-sed -i'' -e "s/githubStars: .*/githubStars: $STARS/" ../src/_data/header2.yml
-sed -i'' -e "s/githubStars: .*/githubStars: $STARS/" ../src/_data/footer2.yml
+sed -i'' -e "s/stars: .*/stars: $STARS/" ../src/_data/github.yml
