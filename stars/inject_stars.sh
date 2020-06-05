3f006c07e1e4927f025293e0768d9c1d36ba4387
Matt Landis <landism@users.noreply.github.com>
2020-06-05 16:49:19 -0400

    docs: fix stars inject for bsd sed (#627)

--- stars/inject_stars.sh
+++ stars/inject_stars.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)
 set -ex
 yarn install
 STARS=$(node stars.js)
-sed -i "s/githubStars: .*/githubStars: $STARS/" ../src/_data/header2.yml
-sed -i "s/githubStars: .*/githubStars: $STARS/" ../src/_data/footer2.yml
+sed -i'' -e "s/githubStars: .*/githubStars: $STARS/" ../src/_data/header2.yml
+sed -i'' -e "s/githubStars: .*/githubStars: $STARS/" ../src/_data/footer2.yml
