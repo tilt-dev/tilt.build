aaa56cda09fe857afe86305f046b2c36070cd6fe
Dan Miller <dmiller@windmill.engineering>
2020-07-29 16:15:48 -0400

    clean up

--- src/assets/js/cta.js
+++ src/assets/js/cta.js
@@ -1,6 +1,12 @@
 const modalTriggers = document.querySelectorAll(".popup-trigger");
 const modalCloseTrigger = document.querySelector(".popup-modal__close");
 const bodyBlackout = document.querySelector(".body-blackout");
+
+const closeModal = (popupModal) => {
+  popupModal.classList.remove("is--visible");
+  bodyBlackout.classList.remove("is-blacked-out");
+};
+
 modalTriggers.forEach((trigger) => {
   trigger.addEventListener("click", () => {
     const { popupTrigger } = trigger.dataset;
@@ -12,14 +18,8 @@ modalTriggers.forEach((trigger) => {
 
     popupModal
       .querySelector(".popup-modal__close")
-      .addEventListener("click", () => {
-        popupModal.classList.remove("is--visible");
-        bodyBlackout.classList.remove("is-blacked-out");
-      });
+      .addEventListener("click", () => closeModal(popupModal));
 
-    bodyBlackout.addEventListener("click", () => {
-      popupModal.classList.remove("is--visible");
-      bodyBlackout.classList.remove("is-blacked-out");
-    });
+    bodyBlackout.addEventListener("click", () => closeModal(popupModal));
   });
 });
