f597f2d462e528ec595361a4893c9c53d738ddfc
Dan Miller <dmiller@tilt.dev>
2020-08-07 13:34:19 -0400

    src: make callback option in modal (#702)
    Co-authored-by: Han Yu <han@tilt.dev>

--- src/assets/js/cta.js
+++ src/assets/js/cta.js
@@ -1,10 +1,22 @@
 const modalTriggers = document.querySelectorAll(".popup-trigger");
-const modalCloseTrigger = document.querySelector(".modal__close");
+const modalCloseTrigger = document.querySelector(".modal-close");
 const bodyBlackout = document.querySelector(".modal-background");
 
 const closeModal = (popupModal) => {
   popupModal.classList.remove("is--visible");
   bodyBlackout.classList.remove("is-blacked-out");
+};
+
+const switchModal = (modal) => {
+  const calendlyDiv = modal.querySelector(".modal-calendly");
+  const callbackDiv = modal.querySelector(".modal-callback");
+  if (calendlyDiv.style.display === "none") {
+    calendlyDiv.style.display = "block";
+    callbackDiv.style.display = "none";
+  } else {
+    calendlyDiv.style.display = "none";
+    callbackDiv.style.display = "block";
+  }
 };
 
 modalTriggers.forEach((trigger) => {
@@ -15,8 +27,16 @@ modalTriggers.forEach((trigger) => {
     bodyBlackout.classList.add("is-blacked-out");
 
     popupModal
-      .querySelector(".modal__close")
-      .addEventListener("click", () => closeModal(popupModal));
+      .querySelectorAll(".modal-close")
+      .forEach((m) =>
+        m.addEventListener("click", () => closeModal(popupModal))
+      );
+
+    popupModal
+      .querySelectorAll(".modal-switchButton")
+      .forEach((m) =>
+        m.addEventListener("click", () => switchModal(popupModal))
+      );
 
     bodyBlackout.addEventListener("click", () => closeModal(popupModal));
   });
