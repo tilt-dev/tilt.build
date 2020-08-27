f3cac7ad90bcfbef5d45730dc7c6697b6d4c054f
Matt Landis <landism@users.noreply.github.com>
2020-08-27 11:26:33 -0400

    tilt.build: fix inject_stars on bsd (#719)

--- stars/inject_stars.sh
+++ stars/inject_stars.sh
@@ -5,4 +5,5 @@ cd $(dirname $0)
 set -ex
 yarn install
 STARS=$(node stars.js)
-sed -i'' -e "s/stars: .*/stars: $STARS/" ../src/_data/github.yml
+sed -i.bak -e "s/stars: .*/stars: $STARS/" ../src/_data/github.yml
+rm -f ../src/_data/github.yml.bak
