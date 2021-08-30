a1769b5e965ccba7ea346cd79bf5c8204655146a
Nick Santos <nick@tilt.dev>
2021-08-30 12:55:54 -0400

    Update api/api.py
    Co-authored-by: Matt Landis <landism@users.noreply.github.com>

--- api/api.py
+++ api/api.py
@@ -442,8 +442,7 @@ def k8s_resource(workload: str = "", new_name: str = "",
     links: one or more links to be associated with this resource in the UI. For more info, see
       `Accessing Resource Endpoints <accessing_resource_endpoints.html#arbitrary-links>`_.
     labels: used to group resources in the Web UI, (e.g. you want all frontend services displayed together, while test and backend services are displayed seperately). A label must start and end with an alphanumeric character, can include ``_``, ``-``, and ``.``, and must be 63 characters or less. For an example, see `Resource Grouping <tiltfile_concepts.html#resource-groups>`_.
-    discovery_strategy: Possible values: '', 'default', 'selectors-only'. When selectors-only is set,
-       Tilt ignores owner references and only uses extra_pod_selectors to find pods.
+    discovery_strategy: Possible values: '', 'default', 'selectors-only'. When '' or 'default', Tilt both uses `extra_pod_selectors` and traces k8s owner references to identify this resource's pods. When 'selectors-only', Tilt uses only `extra_pod_selectors`. 
   """
   pass
 
