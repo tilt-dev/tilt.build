8b89e6e292780804b0968e5866e9b7e50a2e59c7
Nick Santos <nick@tilt.dev>
2021-12-08 09:18:41 -0500

    site: remove jekyll-target-blank and replace it with our own check (#995)
    The jekyll-target-blank's definition of an "external" site is
    non-customizable, and it doesn't seem like the upstream maintainers
    are interested in fixing this.
    https://github.com/keithmifsud/jekyll-target-blank/issues/45

--- src/assets/js/links.js
+++ src/assets/js/links.js
@@ -1,20 +1,33 @@
-// Adapted from
-// http://blog.parkermoore.de/2014/08/01/header-anchor-links-in-vanilla-javascript-for-github-pages-and-jekyll/
-
 (function() {
   const eventListener = () => {
     if (document.readyState !== "complete") {
       return
     }
 
-    const links = document.querySelectorAll('.Docs-content h2[id],.Docs-content h3[id]');
-    links.forEach(el => {
+    // Adapted from
+    // http://blog.parkermoore.de/2014/08/01/header-anchor-links-in-vanilla-javascript-for-github-pages-and-jekyll/
+    // Adds floading header anchors.
+    const headerLinks = document.querySelectorAll('.Docs-content h2[id],.Docs-content h3[id]');
+    headerLinks.forEach(el => {
       // wrap the header contents in a link so that the entire thing is clickable
       const link = document.createElement('a');
       link.href = '#' + el.id;
       link.innerHTML = el.innerHTML;
       el.innerHTML = link.outerHTML;
     })
+
+    // Add _target=_blank to external links.
+    const links = document.querySelectorAll('a')
+    links.forEach(el => {
+      let href = String(el.href)
+      if (href.indexOf('localhost') == -1 &&
+          href.indexOf('tilt.dev') == -1 &&
+          href.indexOf('github.com/tilt-dev/') == -1) {
+        el.target = "_blank"
+        el.rel = "noopener noreferrer"
+      }
+    })
+
     document.removeEventListener('readystatechange', eventListener)
   }
 
