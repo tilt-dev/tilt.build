ad053049c0e8f03b2c67cefddb0cd30fc34cf026
Nick Santos <nick@tilt.dev>
2021-12-17 16:35:26 -0500

    docs: fix toc (#1011)
    * docs: fix toc
    
    * docs: fix toc generation script

--- hack/inject_cli_toc.sh
+++ hack/inject_cli_toc.sh
@@ -2,6 +2,8 @@
 #
 # Regenerates the docs sidebar from the Cobra auto-generated docs (docs/cli/tilt.md)
 
+set -eo pipefail
+
 cd "$(dirname $(dirname "$0"))"
 RESULT=$(python3 hack/gen_cli_toc_helper.py)
 echo "$RESULT" > src/_data/docs.yml
