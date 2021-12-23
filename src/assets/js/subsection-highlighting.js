1bd932e13fc6a3c3aff90d925864d72e55a77e0f
Lian Li <lian@tilt.dev>
2021-12-23 16:27:09 +0100

    docs:  Make it possible to navigate to individual functions in Tiltfile reference from sidebar (#1016)
    * render subpages in sidebar, render subsection headers as closed accordion titles
    
    * adjust css so subsection visibly differs from top level navigation items
    
    * highlight navigation item on click
    
    * open subsection accordion if subsection item is active
    
    * don't close active accordion when navigating in subsection
    
    * adds scripts to generate navigation
    
    * generate docs yaml
    
    * remove max height so accordion is visible in full
    
    * update mobile nav layout
    
    * fix tiltfile reference type links
    
    * render subnav highlighting only after dom is ready
    
    * fix navigation links
    
    * make subsection header background white and caret light gray
    
    * Revert "make subsection header background white and caret light gray"
    
    This reverts commit 8c6068b8c6fdd10aebd6d341cdf9a910ce760c36.
    
    * Revert "Revert "make subsection header background white and caret light gray""
    
    This reverts commit f81fc5c4ac92f65b2974465e0890b5921e762bb9.

---
+++ src/assets/js/subsection-highlighting.js
@@ -0,0 +1,33 @@
+function highlightSubnav(targetUrl) {
+    function activateTarget() {
+        document.querySelectorAll(".Docs-subnav-link").forEach(item => {
+          if (item.href == targetUrl) {
+            item.classList.add("is-active");
+            accordionPanel = item.parentNode.parentNode
+            sectionHeaderButton = accordionPanel.parentNode.firstElementChild.firstChild
+            if (sectionHeaderButton.type == "button" && !!accordionPanel.getAttribute("aria-hidden")) {
+                sectionHeaderButton.click()
+            }
+          } else {
+            item.classList.remove("is-active");
+          }
+        });
+    }
+    
+    // If the active item is not one we clicked, close it first.
+    let activeItem = document.querySelector(".Docs-subnav-link.is-active");
+    if (activeItem && activeItem.href != targetUrl) {
+        activeItem.classList.remove("is-active");
+        setTimeout(activateTarget, 300);
+    } else {
+        activateTarget();
+    }
+}
+
+(function () {
+  function eventListener () {
+    highlightSubnav(document.URL)
+    document.removeEventListener('readystatechange', eventListener)
+  }
+  document.addEventListener('readystatechange', eventListener);
+})();
